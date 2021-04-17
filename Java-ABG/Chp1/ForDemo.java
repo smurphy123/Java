991f576ce0526c9659890fb21172921d7cd56ccd
smurphy123 <smurphysmail@gmail.com>
2021-04-17 08:12:24 -0400

    Repo update

---
+++ Java-ABG/Chp1/ForDemo.java
@@ -0,0 +1,13 @@
+/*
+    Demonstrate the for loops
+*/
+class ForDemo {
+    public static void main(String args[]) {
+        int count;
+
+        for(count = 0; count < 5; count = count + 1) {
+            System.out.println("This is count: " + count);
+        }
+        System.out.println("Done!");
+    }
+}
