936e1e07eefeab2a105182b0f1ca2d71e03fb0fb
smurphy123 <smurphysmail@gmail.com>
2021-04-17 08:29:07 -0400

    Repo update

---
+++ Java-ABG/Chp1/BlockDemo.java
@@ -0,0 +1,18 @@
+/*
+    Demonstrate a block of code.
+*/
+class BlockDemo {
+    public static void main(String args[]) {
+        double i, j, d;
+
+        i = 5;
+        j = 10;
+
+        // the target of this if is a block
+        if(i != 0) {
+            System.out.println("i does not equal zero");
+            d = j / i;
+            System.out.prinln("j / i is " + d);
+        }
+    }
+}
