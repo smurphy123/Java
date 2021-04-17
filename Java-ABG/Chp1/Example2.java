c7325bcd3b3d2b0cfa0d88c122fe539c087263ff
smurphy123 <smurphysmail@gmail.com>
2021-04-16 21:00:03 -0400

    Repo update

---
+++ Java-ABG/Chp1/Example2.java
@@ -0,0 +1,19 @@
+/*
+    This demostates a variable.
+    Call this file Example2.java
+*/
+class Example2 {
+    public static void main(String args[]) {
+        int myVar1; // this declares a variable
+        int myVar2; // this declares another variable
+
+        myVar1 = 1024; // this assigns 1024 ro myVar1
+
+        System.out.println("myVar1 contain " + myVar1);
+
+        myVar2 = myVar1 / 2;
+
+        System.out.print("myVar2 contains myVar1 / 2: ");
+        System.out.println(myVar2);
+    }
+}
