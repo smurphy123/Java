d42d0c5179793b72a369d61cc95cf781bf280931
smurphy123 <smurphysmail@gmail.com>
2021-05-04 21:39:58 -0400

    Repo Update

--- Java-ABG/Chp2/PromDemo.java
+++ Java-ABG/Chp2/PromDemo.java
@@ -5,10 +5,10 @@ class PromDemo {
         int i;
 
         b = 10;
-        i = b * b; // OK, no cast needed
+        i = b * b; // No cast needed. Promoted to an int.
 
         b = 10;
-        b = (byte) (b * b); // cast needed!!
+        b = (byte) (b * b); // cast needed!! (Value too large to be stored in a byte. Needs to be converted to int)
 
         System.out.println("i and b: " + i + " " + b);
     }
