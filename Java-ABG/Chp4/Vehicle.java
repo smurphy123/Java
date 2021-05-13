fdb2947f7bf1860541ba868ee7ae29617a953771
smurphy123 <smurphysmail@gmail.com>
2021-05-12 20:34:08 -0400

    Repo Update

--- Java-ABG/Chp4/Vehicle.java
+++ Java-ABG/Chp4/Vehicle.java
@@ -2,4 +2,9 @@ class Vehicle {
     int passengers; // number of passengers
     int fuelcap;    // fuel capacity in gallons
     int mpg;        // fuel consumption in miles per gallon    
+
+    // Display the range.
+    void range() {
+        System.out.println("Range is " + fuelcap * mpg);
+    }
 }
