45364f626d1de83cf713bb7a460c9fb906996bdd
smurphy123 <smurphysmail@gmail.com>
2021-05-17 18:58:33 -0400

    Repo Update

---
+++ Java-ABG/Chp4/MinMax.java
@@ -0,0 +1,18 @@
+// Find the minimum and maximum values in an array
+class MinMax {
+    public static void main(String args[]) {
+        int nums[] = new int[10];
+        int min, max;
+
+        nums[0] = 99;
+        nums[1] = -10;
+        nums[2] = 100123;
+        nums[3] = 18;
+        nums[4] = -978;
+        nums[5] = 5623;
+        nums[6] = 463;
+        nums[7] = -9;
+        nums[8] = 287;
+        nums[9] = 49;
+    }
+}
