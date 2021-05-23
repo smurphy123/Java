79bb65e26e784e965c12bd6728c55413c45d563c
smurphy123 <smurphysmail@gmail.com>
2021-05-23 16:54:05 -0400

    Repo Update

--- Java-ABG/Chp4/TwoD.java
+++ Java-ABG/Chp4/TwoD.java
@@ -6,7 +6,7 @@ class TwoD {
 
         for(t = 0; t < 3; ++t) {
             for(i = 0; i < 4; ++i) {
-                table[t][i] = (t*4)+i+1;
+                table[t][i] = (t * 4) + i + 1;
                 System.out.print(table[t][i] + " ");
             }
             System.out.println();
