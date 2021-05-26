5f9eb5e21f75ed906fe434812b7fa17c359bd1c9
smurphy123 <smurphysmail@gmail.com>
2021-05-26 10:23:28 -0400

    Repo Update

---
+++ Java-ABG/Chp4/Queue.java
@@ -0,0 +1,27 @@
+class Queue {
+    char q[]; // this array holds the queue
+    int putloc, getloc; // the put and get indices
+    
+    Queue (int size) {
+        q = new char[size]; // allocate memory for queue
+        putloc = getloc = 0;
+    }
+
+    // put a character into the queue
+    void put(char ch) {
+        if(putloc == q.length) {
+            System.out.println(" -  Queue is full.");
+            return;
+        }
+        q[putloc++] = ch;
+    }
+
+    // get a character from the queue
+    char get() {
+        if(getloc == putloc) {
+            System.out.println(" - Queue is empty.");
+            return (char) 0;
+        }
+        return q[getloc++];
+    }
+}
