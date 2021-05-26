50eca34514aca656451cb2ee32ad4a9f4a8d9343
smurphy123 <smurphysmail@gmail.com>
2021-05-25 20:43:22 -0400

    Repo Update

---
+++ Java-ABG/Chp4/AssignARef.java
@@ -0,0 +1,38 @@
+// Assigning array reference variables
+class AssignARef {
+    public static void main(String args[]) {
+        int i;
+
+        int nums1[] = new int[10];
+        int nums2[] = new int[10];
+
+        for(i = 0; i < 10; i++) {
+            nums1[i] = i;
+        }
+
+        for(i = 0; i < 10; i++) {
+            nums2[i] = -i;
+        }
+
+        System.out.print("Here is nums1: ");
+        for(i = 0; i < 10; i++) {
+            System.out.print(nums1[i] + " ");
+        }
+        System.out.println();
+
+        System.out.print("Here is nums2: ");
+        for(i = 0; i < 10; i++) {
+            System.out.print(nums2[i] + " ");
+        }
+        System.out.println();
+
+        nums2 = nums1; // mow nums2 refers to nums 1
+
+        System.out.print("Here is nums2 after assignment: ");
+        for(i = 0; i < 10; i++) {
+            System.out.print(nums2[i] + " ");
+        }
+        System.out.println();
+
+    }
+}
