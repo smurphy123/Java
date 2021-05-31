c85e33ee0e37430ce85a277e5411b8fe9de46335
smurphy123 <smurphysmail@gmail.com>
2021-05-31 17:40:11 -0400

    Repo Update

--- LearningJava/learning-java/src/Main.java
+++ LearningJava/learning-java/src/Main.java
@@ -3,15 +3,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
-        Scanner scanner = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while (isOnRepeat) {
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off of repeat? If so, answer yes.");
+            String userInput = input.next();
 
-        int inputtedNum = scanner.nextInt();
-
-        if (inputtedNum < 5) {
-            System.out.println("Enjoy the good luck a friend brings you");
-        } else {
-            System.out.println("Your shoe selection will make you very happy today");
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
         }
+
+        System.out.println("Playing next song.");
     }
 }
