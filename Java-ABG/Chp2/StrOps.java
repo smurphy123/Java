afcbc31b294d32d8f98aef6ce4fc0eb8e62a3394
smurphy123 <smurphysmail@gmail.com>
2021-06-03 14:34:51 -0400

    Repo Update

--- Java-ABG/Chp2/StrOps.java
+++ Java-ABG/Chp2/StrOps.java
@@ -26,5 +26,22 @@ class StrOps {
         } else {
             System.out.println("str1 does not equal str3");
         }
+
+        result = str1.compareTo(str3);
+        if(result == 0) {
+            System.out.println("str1 and str3 are equal");
+        } 
+        else if (result < 0) {
+            System.out.println("str1 is less than str3");
+        } else {
+            System.out.println("str1 is greater than str3");
+        }
+
+        // assign a new string to str2
+        str2 = "One Two Three One";
+        idx = str2.indexOf("One");
+        System.out.println("Index of first occurrance of One: " + idx);
+        idx = str2.lastIndexOf("One");
+        System.out.println("Index of last occurrance of One: " + idx);
     }
 }
