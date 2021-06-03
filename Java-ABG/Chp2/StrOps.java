6e15aa1e28c79e6d3fa5b57afc3da8837d9c038a
smurphy123 <smurphysmail@gmail.com>
2021-06-02 20:47:35 -0400

    Repo Update

---
+++ Java-ABG/Chp2/StrOps.java
@@ -0,0 +1,24 @@
+// Some String operations
+class StrOps {
+    public static void main(String args[]) {
+        String str1 = "When it comes to Web programming, Java is #1.";
+        String str2 = new String(str1);
+        String str3 = "Java strings are powerful.";
+        int result, idx;
+        char ch;
+
+        System.out.println("Length of str1: " + str1.length());
+
+        // display str1, one char at a time.
+        for(int i = 0; i < str1.length(); i++) {
+            System.out.println(str1.charAt(i));
+        }
+        System.out.println();
+
+        if(str1.equals(str2)) {
+            System.out.println("str1 equals str2");
+        } else {
+            
+        }
+    }
+}
