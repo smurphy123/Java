c888c2fae8d5181fecabdad6034cd470c5c8c52e
smurphy123 <smurphysmail@gmail.com>
2021-06-05 21:29:00 -0400

    Repo Update

--- Java-ABG/Chp2/StringSwitch.java
+++ Java-ABG/Chp2/StringSwitch.java
@@ -4,7 +4,18 @@ public class StringSwitch {
         String command = "cancel";
 
         switch(command) {
-            
+            case "connect":
+                System.out.println("Connecting");
+                break;
+            case "cancel":
+                System.out.println("Canceling");
+                break;
+            case "disconnect":
+                System.out.println("Disconnecting");
+                break;
+            default:
+                System.out.println("Command Error!");
+                break;
         }
     }
 }
