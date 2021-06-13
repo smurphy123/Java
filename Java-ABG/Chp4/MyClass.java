5ae543bdbf8a5e82027d2c86a2d664ac9b96f37c
smurphy123 <smurphysmail@gmail.com>
2021-06-13 15:53:32 -0400

    Repo Update

---
+++ Java-ABG/Chp4/MyClass.java
@@ -0,0 +1,9 @@
+// Local variable type inference with a user-defined class type.
+class MyClass {
+    private int i;
+
+    MyClass(int k) { i= k; }
+
+    int geti() { return i; }
+    void seti(int k) { if(k >= 0) i = k; }
+}
