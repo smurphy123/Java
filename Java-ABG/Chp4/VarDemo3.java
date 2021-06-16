ab0a9fbdb5a8f1480a028c44271da6e857adbaf6
smurphy123 <smurphysmail@gmail.com>
2021-06-15 21:56:23 -0400

    Repo Update

--- Java-ABG/Chp4/VarDemo3.java
+++ Java-ABG/Chp4/VarDemo3.java
@@ -11,5 +11,10 @@ class VarDemo3 {
 
         // Use type inference with iteration variable
         int[] nums = {1, 2, 3, 4, 5, 6 };
+        System.out.print("Values in nums array: ");
+        for(var v : nums) {
+            System.out.print(v + " ");
+        }
+        System.out.println();
     }
 }
