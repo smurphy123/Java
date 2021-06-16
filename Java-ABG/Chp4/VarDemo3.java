ac53240b4b8939af45db4f1bb2549cd6482fe816
smurphy123 <smurphysmail@gmail.com>
2021-06-15 21:55:11 -0400

    Repo Update

---
+++ Java-ABG/Chp4/VarDemo3.java
@@ -0,0 +1,15 @@
+// Use type inference in a for loop.
+class VarDemo3 {
+    public static void main(String args[]) {
+
+        // Use type inference with the loop control variable
+        System.out.println("Values of x: ");
+        for(var x = 2.5; x < 100.0; x = x * 2) {
+            System.out.print(X + " ");
+        }
+        System.out.println();
+
+        // Use type inference with iteration variable
+        int[] nums = {1, 2, 3, 4, 5, 6 };
+    }
+}
