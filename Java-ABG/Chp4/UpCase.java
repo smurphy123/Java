46168804375fd959f12a15920e14321d2621836a
smurphy123 <smurphysmail@gmail.com>
2021-06-19 21:52:25 -0400

    Repo Update

--- Java-ABG/Chp4/UpCase.java
+++ Java-ABG/Chp4/UpCase.java
@@ -3,7 +3,13 @@ class UpCase {
         char ch;
 
         for(int i=0; i<10; i++) {
-            
+            ch = (char)_('a' + i);
+            System.out.print(ch);
+
+            // This statement turrns off the 6th bit.
+            ch = (char) ((int) ch & 65503); // ch is now uppercase
+
+            System.out.print(ch + " ");
         }
     }
 }
