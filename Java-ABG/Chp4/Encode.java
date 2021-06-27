5ccf85b8b3d25fadffe22dc53433f57e60d0837e
smurphy123 <smurphysmail@gmail.com>
2021-06-26 21:36:31 -0400

    Repo Update

--- Java-ABG/Chp4/Encode.java
+++ Java-ABG/Chp4/Encode.java
@@ -11,14 +11,14 @@ class Encode {
 
         // encode the message
         for (int i = 0; i < msg.length(); i++) {
-            encmsg = encmsg + (char) (msg.charAt(i) ^ key);
+            encmsg = encmsg + (char) (msg.charAt(i) ^ key); // XOR w/key
         }
         System.out.print("Encoded message: ");
         System.out.println(encmsg);
 
         // decode the message
         for(int i = 0; i < msg.length(); i++) {
-            decmsg = decmsg + (char) (encmsg.charAt(i) ^ key);
+            decmsg = decmsg + (char) (encmsg.charAt(i) ^ key); //XOR w/key
         }
         System.out.print("Decoded message: ");
         System.out.println(decmsg);
