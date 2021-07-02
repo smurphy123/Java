ffc614bbcea402e308766cdb7dea4a5e649fad34
smurphy123 <smurphysmail@gmail.com>
2021-07-01 21:56:56 -0400

    Repo Update

--- Java-ABG/Chp5/ShiftDemo.java
+++ Java-ABG/Chp5/ShiftDemo.java
@@ -14,6 +14,19 @@ class ShiftDemo {
             System.out.println();
             val = val << 1; // left shift
         }
-        System.out.println();;
+        System.out.println();
+
+        val = 128;
+        for (int i = 0; i < 8; i++) {
+            for (int t = 128; t > 0; t = t/2) {
+                if((val & t) != 0) {
+                    System.out.print("1 ");
+                } else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+            val = val >> 1; // right shift
+        }
     }
 }
