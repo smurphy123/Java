d8e4dfae16b120bab75b7db2c0bcdc14e8488dee
smurphy123 <smurphysmail@gmail.com>
2021-07-04 23:08:44 -0400

    Repo Update

--- Java-ABG/Chp5/ShowBitsDemo.java
+++ Java-ABG/Chp5/ShowBitsDemo.java
@@ -2,5 +2,12 @@
 class ShowBitsDemo {
     public static void main(String args[]) {
         ShowBits b = new ShowBits(8);
+        ShowBits i = new ShowBits(32);
+        ShowBits li = new ShowBits(64);
+        System.out.println("123 in binary: ");
+        b.show(123);
+
+        System.out.println("\n87987 in binary: ");
+        i.show(87987);
     }
 }
