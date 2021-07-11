6b3556850723835117ae0deb3b47f2db8b53fd93
smurphy123 <smurphysmail@gmail.com>
2021-07-10 22:02:17 -0400

    Repo Update

--- Java-ABG/Chp6/AccessDemo.java
+++ Java-ABG/Chp6/AccessDemo.java
@@ -6,6 +6,13 @@ class AccessDemo {
            through it's accessor methods. */
         ob.setAlpha(-99);
         System.out.println("ob.alpha is " + ob.getAlpha);
+
+        // You cannot access alpha like this: 
+        // ob.alpha = 10; (WRONG! alpha is private!)
+
+        // These are OK because beta and gamma are public
+        ob.beta = 88;
+        ob.gamma = 99;
     }
     
 }
