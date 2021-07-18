a7319fb9f16530dfac17f29dcac89f0053d24a14
smurphy123 <smurphysmail@gmail.com>
2021-07-18 18:49:19 -0400

    Repo Update

--- JavaTut/src/HelloWorld.java
+++ JavaTut/src/HelloWorld.java
@@ -7,20 +7,9 @@ public class HelloWorld {
 
     public static void main(String[]  args) {
 
-        int smInt = 10;
-        long smLong = smInt;
-
-        double cDbl = 1.234;
-        int cInt = (int) cDbl;
-        System.out.println(cInt);
-
-        long bigLong = 2147486470L;
-        int bInt = (int) bigLong;
-        System.out.println(bInt);
-
-        String favNum = Double.toString(1.618);
-        int strInt = Integer.parseInt("10");
-        System.out.println(strInt);
+        int incMe = 0;
+        System.out.println("incMe : " + (incMe++));
+        System.out.println("incMe : " + (++incMe));
 
     }
 }
