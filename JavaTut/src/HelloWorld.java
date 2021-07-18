7f0be67ea58e62f4987d4af3ab3343997628bc77
smurphy123 <smurphysmail@gmail.com>
2021-07-18 18:38:11 -0400

    Repo Update

--- JavaTut/src/HelloWorld.java
+++ JavaTut/src/HelloWorld.java
@@ -9,20 +9,14 @@ public class HelloWorld {
 
     public static void main(String[]  args) {
 
-        System.out.println("Byte Max: " + Byte.MAX_VALUE);
-        System.out.println("Byte Min: " + Byte.MIN_VALUE);
-        System.out.println("Short Max: " + Short.MAX_VALUE);
-        System.out.println("Short Min: " + Short.MAX_VALUE);
-        System.out.println("Char Max: " + Character.MAX_VALUE);
-        System.out.println("Char Min: " + Character.MAX_VALUE);
-        System.out.println("Int Max: " + Integer.MAX_VALUE);
-        System.out.println("Int Min: " + Integer.MAX_VALUE);
-        System.out.println("Float Max: " + Float.MAX_VALUE);
-        System.out.println("Float Min: " + Float.MAX_VALUE);
-        System.out.println("Double Max: " + Double.MAX_VALUE);
-        System.out.println("Double Min: " + Double.MAX_VALUE);
-        System.out.println("Long Max: " + Long.MAX_VALUE);
-        System.out.println("Long Min: " + Long.MAX_VALUE);
+        boolean happy = true;
+        char a = 'a';
+        float fNum = 1.11111111111111F;
+        float fNUM2 = 1.111111111111F;
+        System.out.println("Float : " + fNum + fNUM2);
+
+        double thousand = 1e+3;
+        System.out.println(thousand);
 
 
     }
