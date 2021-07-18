44aad57415e351ea8c6d9fedca664b213264d4ac
smurphy123 <smurphysmail@gmail.com>
2021-07-18 18:34:50 -0400

    Repo Update

---
+++ JavaTut/src/HelloWorld.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.util.stream.IntStream;
+
+public class HelloWorld {
+
+    static Scanner sc = new Scanner(System.in);
+
+    final double SHORTPI = 3.14159;
+
+    public static void main(String[]  args) {
+
+        System.out.println("Byte Max: " + Byte.MAX_VALUE);
+        System.out.println("Byte Min: " + Byte.MIN_VALUE);
+        System.out.println("Short Max: " + Short.MAX_VALUE);
+        System.out.println("Short Min: " + Short.MAX_VALUE);
+        System.out.println("Char Max: " + Character.MAX_VALUE);
+        System.out.println("Char Min: " + Character.MAX_VALUE);
+        System.out.println("Int Max: " + Integer.MAX_VALUE);
+        System.out.println("Int Min: " + Integer.MAX_VALUE);
+        System.out.println("Float Max: " + Float.MAX_VALUE);
+        System.out.println("Float Min: " + Float.MAX_VALUE);
+        System.out.println("Double Max: " + Double.MAX_VALUE);
+        System.out.println("Double Min: " + Double.MAX_VALUE);
+        System.out.println("Long Max: " + Long.MAX_VALUE);
+        System.out.println("Long Min: " + Long.MAX_VALUE);
+
+
+    }
+}
