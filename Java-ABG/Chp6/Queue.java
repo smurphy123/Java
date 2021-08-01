cc5bddba13366c3fbe5e4e75fcf839f39304e2af
smurphy123 <smurphysmail@gmail.com>
2021-07-31 23:15:17 -0400

    Repo Update

---
+++ Java-ABG/Chp6/Queue.java
@@ -0,0 +1,6 @@
+// An imporoved queue class for characters.
+class Queue {
+    // these members are now private
+    private char q[]; // this array hold the queue
+    private int putloc, getloc; // the put and get indices
+}
