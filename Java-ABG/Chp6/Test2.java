5101db6199cb6013540bcf8a8c73dd9b89f35b18
smurphy123 <smurphysmail@gmail.com>
2021-08-07 21:05:48 -0400

    Repo Update

---
+++ Java-ABG/Chp6/Test2.java
@@ -0,0 +1,15 @@
+// Objects are passed through their references.
+class Test2 {
+    int a, b;
+
+    Test2(int i, int j) {
+        a = i;
+        b = j;
+    }
+    /* Pass an object. Now, ob.a and ob.b in object
+    used in the call will be changed */
+    void change(Test ob) {
+        ob.a = ob.a + ob.b;
+        ob.b = -ob.b;
+    }
+}
