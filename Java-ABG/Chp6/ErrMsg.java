18da8c0744fdf6c1e20e5ef7e34870be30647f9f
smurphy123 <smurphysmail@gmail.com>
2021-08-10 21:38:52 -0400

    Repo Update

---
+++ Java-ABG/Chp6/ErrMsg.java
@@ -0,0 +1,7 @@
+class ErrMsg {
+    public static void main(String args[]) {
+        ErrorMsg err = new ErrorMsg();
+        System.out.println(err.getErrorMsg(2));
+        System.out.println(err.getErrorMsg(19));
+    }    
+}
