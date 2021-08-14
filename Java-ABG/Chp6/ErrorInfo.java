8a35aff2a5ae6b6a76311684171532326866c83f
smurphy123 <smurphysmail@gmail.com>
2021-08-13 22:08:32 -0400

    Repo Update

---
+++ Java-ABG/Chp6/ErrorInfo.java
@@ -0,0 +1,11 @@
+class ErrorInfo {
+    String msgs[] = {
+        "Output Error",
+        "Input Error",
+        "Disk Full",
+        "Index Out-Of-Bounds"
+    };
+    int howbad[] = { 3,3,2,4 };
+
+    
+}
