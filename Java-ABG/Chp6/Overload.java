ddc73a638004a124660a0c4d63a5add002b5c520
smurphy123 <smurphysmail@gmail.com>
2021-08-14 21:57:34 -0400

    Repo Update

--- Java-ABG/Chp6/Overload.java
+++ Java-ABG/Chp6/Overload.java
@@ -1,5 +1,12 @@
 class Overload {
-    void ovlDemo() {
+    void ovlDemo() { // First version
         System.out.println("No parameters");
     }
+
+    // Overload ovlDemo for one integer parameter
+    void ovlDemo(int a) { // Second version
+        System.out.println("One parameter: " +  a);
+    }
+
+    // Overload ovlDemo for two integer parameters.
 }
