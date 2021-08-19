bc5e0bed8787c37c8fc02b8adddb61bc0603b4a3
smurphy123 <smurphysmail@gmail.com>
2021-08-18 20:48:33 -0400

    Repo Update

---
+++ Codecademy/SavingsAccount.java
@@ -0,0 +1,18 @@
+public SavingsAccount{
+    double balance;
+    public SavingsAccount(double startingBalance){
+      balance = startingBalance;
+    }
+   
+    public void deposit(double amountToDeposit){
+       //Add amountToDeposit to the balance
+    }
+   
+    public void withdraw(double amountToWithdraw){
+       //Subtract amountToWithdraw from the balance
+    }
+   
+    public static void main(String[] args){
+   
+    }
+  }
