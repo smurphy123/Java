17bb91493fd866891724de47a53c71746a846b95
smurphy123 <smurphysmail@gmail.com>
2021-08-20 22:21:06 -0400

    Repo Update

--- Codecademy/CarLot.java
+++ Codecademy/CarLot.java
@@ -1,5 +1,6 @@
 class CarLot {
     Car carInLot;
+    
     public CarLot(Car givenCar) {
         carInLot = givenCar;
     }
@@ -7,12 +8,5 @@ class CarLot {
     public Car returnACar() {
         // return Car object
         return carInLot;
-    }
- 
-    public static void main(String[] args) {
-        Car myCar = new Car("red", 70);
-        System.out.println(myCar); 
-        CarLot myCarLot = new CarLot(myCar);
-        System.out.println(myCarLot.returnACar());
     }
 }
