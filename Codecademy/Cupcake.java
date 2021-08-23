bf84d3ff781376112ded98835252c3b8e9a310ee
smurphy123 <smurphysmail@gmail.com>
2021-08-22 21:01:33 -0400

    Repo Update

---
+++ Codecademy/Cupcake.java
@@ -0,0 +1,9 @@
+class Cupcake {
+    String flavor;
+    boolean sprinkles;
+   
+    public Cupcake(String type, boolean hasSprinkles) {
+      flavor = type;
+      sprinkles = hasSprinkles;
+    }
+  }
