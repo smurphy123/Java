15764f3541f4ff391dbcd656c49adc053009ae77
smurphy123 <smurphysmail@gmail.com>
2021-08-23 15:03:59 -0400

    Repo Update

--- Codecademy/Car.java
+++ Codecademy/Car.java
@@ -1,17 +1,24 @@
 class Car {
  
     String color;
+    int year;
  
     public Car(String carColor) {
         color = carColor;
     }
+
+    public Car(String carColor, int carYear) {
+        color = carColor;
+        year = carYear;
+    }
+
  
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Car myCar = new Car("red");
         System.out.println(myCar);
     }
  
-   public String toString(){
+   public String toString() {
        return "This is a " + color + " car!";
    }
 }
