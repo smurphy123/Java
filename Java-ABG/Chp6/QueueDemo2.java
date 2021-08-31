6c588b8985528ee04dc0e4527c7513aa143ef149
smurphy123 <smurphysmail@gmail.com>
2021-08-30 20:21:24 -0400

    Repo Update

---
+++ Java-ABG/Chp6/QueueDemo2.java
@@ -0,0 +1,29 @@
+public class QueueDemo2 {
+    public static void main(String args[]) {
+        // construct 10-element empty queue
+        Queue q1 = new Queue(10);
+
+        char name[] = {'T', 'o', 'm'};
+        // construct queue from array
+        Queue q2 = new Queue(name);
+
+        char ch;
+        int i;
+
+        // put some characters into q1
+        for(i=0; i < 10; i++) {
+            q1.put((char) ('A' + i));
+        }
+
+        // construct queue from another queue
+        Queue q3 = new Queue(q1);
+
+        // Show the queues
+        System.out.print("Contants of q1: ");
+        for(i = 0; i < 10; i++) {
+            ch = q1.get();
+
+            System.out.print(ch);
+        }
+    }
+}
