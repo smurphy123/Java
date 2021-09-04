f5d336ca464ca3083856c1b8f1eac03f8dbbd084
smurphy123 <smurphysmail@gmail.com>
2021-09-03 22:16:17 -0400

    Repo Update

---
+++ Codecademy/AytoboxingAndUnboxing.java
@@ -0,0 +1,19 @@
+public class AutoboxingAndUnboxing {
+	public static void main(String[] args) {
+
+		double doubleVal = 11.56;
+		
+		//Autobox the value of `doubleVal` into a Double wrapper object called `wrapper`
+    Double wrapper = doubleVal;
+
+		//Create a new Integer wrapper object containing the value 20 called `intWrapper`
+		Integer intWrapper = new Integer(20);
+		
+		//Unbox the value stored in `intWrapper` into a int variable called `resultInt`
+  int resultInt = intWrapper;
+    
+    //Feel free to print any of your variables
+
+
+	}
+}
