e9c6acaa0a4bf7693c8337330659428d1be2434c
smurphy123 <smurphysmail@gmail.com>
2021-09-04 23:22:59 -0400

    Repo Update

--- Java-ABG/Chp6/SDemo.java
+++ Java-ABG/Chp6/SDemo.java
@@ -6,5 +6,8 @@ class SDemo {
         // Each object has its own copy of an instance variable
         ob1.x = 10;
         ob2.x = 20;
+        System.out.println("Of course, ob1.x and ob2.x are independent.")
+        System.out.println("ob1.x: " + ob1.x + "\nob2.x: " + ob2.x);
+        System.out.println();
     }    
 }
