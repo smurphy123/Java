7a24dc640be1cf0f6dea06b2a383e2fd7c5a917a
smurphy123 <smurphysmail@gmail.com>
2021-09-05 09:53:23 -0400

    Repo Update

---
+++ Java-ABG/Chp6/StaticMeth.java
@@ -0,0 +1,9 @@
+// Use a static method
+class StaticMeth {
+    static int val = 1024; // a static variiable
+    
+    // a static method
+    static int valDiv2() {
+        return val/2;
+    }
+}
