98143769c423461802552fdcaecf1116cac0ebdb
smurphy123 <smurphysmail@gmail.com>
2021-09-05 10:03:50 -0400

    Repo Update

---
+++ Java-ABG/Chp6/StaticBlock.java
@@ -0,0 +1,15 @@
+// Use a staic block
+class StaticBlock {
+    static double rootOf2;
+    static double rootOf3;
+
+    static {
+        System.out.println("Inside static block.");
+        rootOf2 = Math.sqrt(2.0);
+        rootOf3 = Math.sqrt(3.0);
+    }
+
+    StaticBlock(String msg) {
+        System.out.println(msg);
+    }
+}
