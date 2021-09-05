370f78baaad6ba8721b5ca4b2dedf9ab050872ad
smurphy123 <smurphysmail@gmail.com>
2021-09-04 23:27:26 -0400

    Repo Update

--- Java-ABG/Chp6/SDemo.java
+++ Java-ABG/Chp6/SDemo.java
@@ -10,6 +10,20 @@ class SDemo {
         System.out.println("ob1.x: " + ob1.x + "\nob2.x: " + ob2.x);
         System.out.println();
 
-        
+        // Each object shared one copy of a static variable
+        System.out.println("The static variable y is shared.");
+        StaticDemo.y = 19;
+        System.out.println("Set StaticDemo.y to 19.");
+
+        System.out.println("ob1.sum(): " + ob1.sum());
+        System.out.println("ob2.sum(): " + ob2.sum());
+        System.out.println();
+
+        StaticDemo.y = 100;
+        System.out.println("Change StaticDemo.y to 100" );
+
+        System.out.println("ob1.sum(): " + ob1.sum());
+        System.out.println("ob2.sum(): " + ob2.sum());
+        System.out.println();
     }    
 }
