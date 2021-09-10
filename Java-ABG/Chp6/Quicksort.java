eb835ecc07a674af6e4f4dc7bb00925555cb86cd
smurphy123 <smurphysmail@gmail.com>
2021-09-09 20:08:09 -0400

    Repo Update

---
+++ Java-ABG/Chp6/Quicksort.java
@@ -0,0 +1,8 @@
+// sample version of the Quicksort
+class Quicksort {
+    
+    // Set up a call to the actual Quicksortmethod.
+    static void qsort(char items[]) {
+        qs(items, 0, items.length-1);
+    }
+}
