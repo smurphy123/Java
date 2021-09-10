7a813aa61b68d974abf924731f3a29f5eb9f0ec5
smurphy123 <smurphysmail@gmail.com>
2021-09-09 20:13:23 -0400

    Repo Update

--- Java-ABG/Chp6/Quicksort.java
+++ Java-ABG/Chp6/Quicksort.java
@@ -14,6 +14,19 @@ class Quicksort {
         i = left; j = right;
         x = items[(left+right)/2];
 
-        
+        do {
+            while((items[i] < x) && (i < right)) {
+                i++;
+            }
+            while((x < items[j]) && (j > left)) {
+                j--;
+            }
+
+            if(i <= j) {
+                y = items[i];
+                items[j] = y;
+                i++; j--;
+            }
+        } while(i <= j);
     }
 }
