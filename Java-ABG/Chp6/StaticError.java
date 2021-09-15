09faa702d8e2549f04087c1c351f2204a3dea986
smurphy123 <smurphysmail@gmail.com>
2021-09-14 21:47:10 -0400

    Repo Update

--- Java-ABG/Chp6/StaticError.java
+++ Java-ABG/Chp6/StaticError.java
@@ -3,8 +3,8 @@ class StaticError {
     static int val = 1024; // static variable
 
     /* Error! Can't access a non-static variable
-    from within a static method. */
+    from within a static method. 
     static int valDivDenom() {
-        return val/denom; //won't compile
-    }
+       return val/denom; //won't compile
+    } */
 }
