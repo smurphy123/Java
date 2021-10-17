28aed45cb02045d7cebfd99ec010949aa04a7e2a
smurphy123 <smurphysmail@gmail.com>
2021-10-16 23:44:30 -0400

    Repo Update

--- Java-ABG/Chp6/StaticBlock.java
+++ Java-ABG/Chp6/StaticBlock.java
@@ -3,7 +3,8 @@ class StaticBlock {
     static double rootOf2;
     static double rootOf3;
 
-    static {
+    /* This block is executed when the class is loaded */
+    static { 
         System.out.println("Inside static block.");
         rootOf2 = Math.sqrt(2.0);
         rootOf3 = Math.sqrt(3.0);
