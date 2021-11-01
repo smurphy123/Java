b38eaa7881ca160247f06a21d13f4eedc4b92f98
smurphy123 <smurphysmail@gmail.com>
2021-10-31 20:23:38 -0400

    Repo Update

--- Java-ABG/Chp6/LocalClassDemo.java
+++ Java-ABG/Chp6/LocalClassDemo.java
@@ -1,6 +1,22 @@
 // Use Showbits as a local class.
 class LocalClassDemo {
     public static void main(String args[]) {
-        
+
+        // An inner class version of Showbits.
+        class ShowBits {
+            int numbits;
+        }
+
+        void show(long val) {
+            long mask = 1;
+
+            // left-shift a 1 into the proper position
+            mask <<= numbits-1;
+
+            int spacer = 0;
+            for(; mask != 0; mask >>>= 1) {
+                if((val & mask) != 0) System.out.print("1");
+            }
+        }
     }
 }
