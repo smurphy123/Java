46cac06364492cea023c81a4df92949da13c3dfb
smurphy123 <smurphysmail@gmail.com>
2021-10-31 20:28:10 -0400

    Repo Update

--- Java-ABG/Chp6/LocalClassDemo.java
+++ Java-ABG/Chp6/LocalClassDemo.java
@@ -15,8 +15,24 @@ class LocalClassDemo {
 
             int spacer = 0;
             for(; mask != 0; mask >>>= 1) {
-                if((val & mask) != 0) System.out.print("1");
+                if((val & mask) != 0) { 
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+                spacer++;
+                if((spacer % 8) == 0) {
+                    System.out.print(" ");
+                    spacer = 0;
+                }
             }
+            System.out.println();
         }
+    }
+
+    for(byte b  = 0; b < 10; b++) {
+        ShowBits byteval = new ShowBits(8);
+        System.out.print(b + " in binary: ");
+        byteval.show(b);
     }
 }
