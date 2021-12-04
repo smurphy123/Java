ebeb49747d8b4fb62e01aa6d1c19b43118c7088c
smurphy123 <smurphysmail@gmail.com>
2021-12-04 15:02:27 -0500

    Repo Update

---
+++ Java-ABG/Chp6/VarLogs4.java
@@ -0,0 +1,14 @@
+// Varlogs, overloading and ambiguity
+//
+// NOTE: This program contains and error an will not compile!
+
+class VarLogs4 {
+
+    static void VaTest(int v) {
+
+    }
+
+    static vaTest(boolean v) {
+
+    }
+}
