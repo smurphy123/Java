108a440b624434b8ced2ccaf36e02db2ca05b29c
smurphy123 <smurphysmail@gmail.com>
2021-12-06 09:53:16 -0500

    Repo Update

---
+++ Codecademy/Tree.java
@@ -0,0 +1,17 @@
+// Define your tree class in this file!
+public class Tree {
+    public static void main(String[] args) {
+      /* This program will allow you to introduce yourself
+         and plant a tree to the screen. */
+      System.out.println("Hey there, I’m Ariel and I’m learning to code in Java!");
+      System.out.println();
+      System.out.println("I'm going to plant a tree today!");
+      System.out.println();
+      System.out.println("  *  ");
+      System.out.println(" *** ");
+      System.out.println(" *** ");
+      System.out.println("  *  ");
+      System.out.println("  *  "); 
+  
+    }
+  }
